f87474f8cb9b2e22d89383815f9180a1350d62c6
fujimoto <yuukikun666@gmail.com>
2022-09-23 01:28:07 +0900

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'top' => 'home#top'
+  get '/' => 'home#top'
   get 'about' => 'home#about'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
